9d0adc481a346184a241a580db67ad0c11f5682f
decadent-cuB3r <cub3r.ezra.yu@gmail.com>
2022-11-23 01:27:56 +0800

    Login navigation to signup

--- 9Knights/Screens/Login.swift
+++ 9Knights/Screens/Login.swift
@@ -11,119 +11,123 @@ struct Login: View {
     @State private var email = ""
     @State private var password = ""
     @State private var passwordCheck = false
+    @State private var path = NavigationPath()
 
     var body: some View {
-        ZStack {
-            Circle()
-                .fill(LinearGradient(gradient: Gradient(colors: [Color.white, Color("ECECEC")]), startPoint: .leading, endPoint: .trailing))
-                .rotationEffect(.degrees(40))
-                .offset(x: 180, y: -250)
-                .frame(width: 500, height: 500)
-            Circle()
-                .fill(LinearGradient(gradient: Gradient(colors: [Color.white, Color("ECECEC")]), startPoint: .leading, endPoint: .trailing))
-                .rotationEffect(.degrees(50))
-                .offset(x: -180, y: 350)
-            VStack{
-                Image("logoImage")
-                Image("logoWord")
-                TextField("電子信箱", text: $email)
-                    .padding(EdgeInsets(top: 0, leading: 6, bottom: 0, trailing: 6))
-                    .frame(width: 300, height: 50)
-                    .foregroundColor(Color("999999"))
-                    .background(Color("ECECEC"))
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.black, lineWidth: 1)
+        NavigationStack(path: $path) {
+            ZStack {
+                Circle()
+                    .fill(LinearGradient(gradient: Gradient(colors: [Color.white, Color("ECECEC")]), startPoint: .leading, endPoint: .trailing))
+                    .rotationEffect(.degrees(40))
+                    .offset(x: 180, y: -250)
+                    .frame(width: 500, height: 500)
+                Circle()
+                    .fill(LinearGradient(gradient: Gradient(colors: [Color.white, Color("ECECEC")]), startPoint: .leading, endPoint: .trailing))
+                    .rotationEffect(.degrees(50))
+                    .offset(x: -180, y: 350)
+                VStack{
+                    Image("logoImage")
+                    Image("logoWord")
+                    TextField("電子信箱", text: $email)
+                        .padding(EdgeInsets(top: 0, leading: 6, bottom: 0, trailing: 6))
+                        .frame(width: 300, height: 50)
+                        .foregroundColor(Color("999999"))
+                        .background(Color("ECECEC"))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.black, lineWidth: 1)
+                                
+                        )
+                        .padding(.bottom,10)
+                    TextField("密碼", text: $password)
+                        .padding(EdgeInsets(top: 0, leading: 6, bottom: 0, trailing: 6))
+                        .frame(width: 300, height: 50)
+                        .background(Color("ECECEC"))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 10)
+                                .stroke(Color.black, lineWidth: 1)
+                        )
+                    HStack{
+                        Button{
+                            passwordCheck.toggle()
                             
-                    )
-                    .padding(.bottom,10)
-                TextField("密碼", text: $password)
-                    .padding(EdgeInsets(top: 0, leading: 6, bottom: 0, trailing: 6))
-                    .frame(width: 300, height: 50)
-                    .background(Color("ECECEC"))
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10)
-                            .stroke(Color.black, lineWidth: 1)
+                        } label: {
+                            HStack{
+                                Image(systemName: passwordCheck ? "checkmark.square": "square")
+                                    .padding(.trailing, -5)
+                                Text("保持登入")
+                                    .font(.system(size: 14))
+                                    .foregroundColor(.black)
+                            }
+                        }.buttonStyle(StaticButtonStyle())
+                        Spacer()
+                        Button{
                             
-                    )
-                    
-                HStack{
-                    Button{
-                        passwordCheck.toggle()
-                        
-                    } label: {
-                        HStack{
-                            Image(systemName: passwordCheck ? "checkmark.square": "square")
-                                .padding(.trailing, -5)
-                            Text("保持登入")
+                        }label: {
+                            Text("忘記密碼")
                                 .font(.system(size: 14))
                                 .foregroundColor(.black)
+                                .underline()
                         }
-                    }.buttonStyle(StaticButtonStyle())
-                    Spacer()
-                    Button{
-                        
-                    }label: {
-                        Text("忘記密碼")
-                            .font(.system(size: 14))
-                            .foregroundColor(.black)
-                            .underline()
-                    }
-                }.frame(width: 300)
-                    .padding(EdgeInsets(top: 10, leading: 0, bottom: 20, trailing: 0))
-                
-                //Login Button
-                Button {
-                    //Login func
-                } label: {
-                    Spacer()
-                    Text("登入")
-                        .foregroundColor(.white)
-                    Spacer()
-                }
-                .frame(width: 300, height: 50)
-                .background(Color("43413F"))
-                .cornerRadius(20)
-                .padding(.bottom, 20)
-                
-                Text("或")
-                    .font(.system(size: 14))
-                    .foregroundColor(Color("999999"))
-                    .padding(.bottom,20)
-                //Facebook Login Button
-
-                Button {
-                    //Login with facebook func
-                } label: {
-                    Spacer()
-                    Text("登入facebook繼續")
-                        .foregroundColor(.white)
-                    Spacer()
-                }
-                .frame(width: 300, height: 50)
-                .background(Color("4267B2"))
-                .cornerRadius(20)
-                HStack{
-                    Text("還沒有帳號？")
-                        .font(.system(size: 14))
-                    Button{
-                        //signup
+                    }.frame(width: 300)
+                        .padding(EdgeInsets(top: 10, leading: 0, bottom: 20, trailing: 0))
+                    
+                    //Login Button
+                    Button {
+                        //Login func
                     } label: {
-                        Text("點此註冊新帳號")
-                            .font(.system(size: 14))
-                            .foregroundColor(.black)
-                            .underline()
+                        Spacer()
+                        Text("登入")
+                            .foregroundColor(.white)
+                        Spacer()
                     }
-                }.padding(10)
+                    .frame(width: 300, height: 50)
+                    .background(Color("43413F"))
+                    .cornerRadius(20)
+                    .padding(.bottom, 20)
+                    
+                    Text("或")
+                        .font(.system(size: 14))
+                        .foregroundColor(Color("999999"))
+                        .padding(.bottom,20)
+                    //Facebook Login Button
 
+                    Button {
+                        //Login with facebook func
+                    } label: {
+                        Spacer()
+                        Text("登入facebook繼續")
+                            .foregroundColor(.white)
+                        Spacer()
+                    }
+                    .frame(width: 300, height: 50)
+                    .background(Color("4267B2"))
+                    .cornerRadius(20)
+                    HStack{
+                        Text("還沒有帳號？")
+                            .font(.system(size: 14))
+                        NavigationLink(destination: Login()) {
+                            Button{
+                                path.append("Signup")
+                            } label: {
+                                Text("點此註冊新帳號")
+                                    .font(.system(size: 14))
+                                    .foregroundColor(.black)
+                                    .underline()
+                            }.navigationDestination(for: String.self) { view in
+                                switch view {
+                                case "Signup":
+                                    Signup()
 
-            }
-        }.ignoresSafeArea().fixedSize()
-    }
-    
-    //function
-    func login() {
-        
+                                default:
+                                    Text("Unknown")
+                                }
+                            }
+                        }
+                    }
+                }
+            }.ignoresSafeArea().fixedSize()
+        }
     }
     
 }
--- 9Knights/Screens/Signup.swift
+++ 9Knights/Screens/Signup.swift
@@ -8,13 +8,107 @@
 import SwiftUI
 
 struct Signup: View {
+    @State private var userName = ""
+    @State private var email = ""
+    @State private var password = ""
+    @State private var passwordCheck = ""
+    @State private var positionY = 700
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+
+    var btnBack : some View {
+        Button{
+            self.presentationMode.wrappedValue.dismiss()
+        } label: {
+            Spacer()
+            Image("whiteArrow")
+                .foregroundColor(.white)
+            Spacer()
+        }
+        .frame(width: 50, height: 50)
+        .background(Color("DA4F40"))
+        .cornerRadius(50)
+        .padding(EdgeInsets(top: 0, leading: 20, bottom: 60, trailing: 0))
+    }
+    
     var body: some View {
-        ZStack{
+        ZStack(alignment: .bottom){
+            Color("FAF3EA")
+                .ignoresSafeArea(.all)
+            
             VStack{
-                Text("註冊新帳號")
-                    .font(.largeTitle)
-            }.padding(.top, 20)
-        }.background(Color("FAF3EA"))
+                HStack{
+                    btnBack
+                    Spacer()
+                }.padding(.top,80)
+                VStack{
+                    VStack{
+                        Text("註冊新帳號")
+                            .font(.system(size: 32,
+                                          weight: .bold))
+                        TextField("使用者名稱", text: $userName)
+                            .padding(EdgeInsets(top: 0, leading: 6, bottom: 0, trailing: 6))
+                            .frame(width: 300, height: 50)
+                            .background(Color("ECECEC"))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.black, lineWidth: 1)
+                            ).padding(10)
+                        TextField("電子信箱", text: $email)
+                            .padding(EdgeInsets(top: 0, leading: 6, bottom: 0, trailing: 6))
+                            .frame(width: 300, height: 50)
+                            .background(Color("ECECEC"))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.black, lineWidth: 1)
+                            ).padding(10)
+                        TextField("密碼", text: $password)
+                            .padding(EdgeInsets(top: 0, leading: 6, bottom: 0, trailing: 6))
+                            .frame(width: 300, height: 50)
+                            .background(Color("ECECEC"))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.black, lineWidth: 1)
+                            ).padding(10)
+                        TextField("確認密碼", text: $passwordCheck)
+                            .padding(EdgeInsets(top: 0, leading: 6, bottom: 0, trailing: 6))
+                            .frame(width: 300, height: 50)
+                            .background(Color("ECECEC"))
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10)
+                                    .stroke(Color.black, lineWidth: 1)
+                            ).padding(10)
+                        Button {
+                            //Login func
+                        } label: {
+                            Spacer()
+                            Text("登入")
+                                .foregroundColor(.white)
+                            Spacer()
+                        }
+                        .frame(width: 300, height: 50)
+                        .background(Color("43413F"))
+                        .cornerRadius(20)
+                        .padding(EdgeInsets(top: 10, leading: 0, bottom: 70, trailing: 0))
+                    }
+                   
+                    .frame(width: 390, height: 700)
+                    .background(.white)
+                    .cornerRadius(60)
+                    //                    .onAppear(){
+                    //                        positionY += 200
+                    //                    }
+                }
+                .offset(y:CGFloat(positionY))
+                .animation(.spring(), value: positionY)
+                .onAppear(){
+                    positionY -= 700
+                }
+            }
+                
+            
+             
+        }
+        .navigationBarBackButtonHidden(true)
     }
 }
 struct Signup_Previews: PreviewProvider {
